15f864fbd3196192c5a1531c326debf05cdad69e
CYBER HUNTER BD <144377530+tahidul1234@users.noreply.github.com>
2023-09-08 16:21:49 +0600

    Add files via upload

---
+++ LOGI.py
@@ -0,0 +1,26 @@
+print("""                                                                                                                                  
+                                                                                                                                  
+MMMMMMMM               MMMMMMMMRRRRRRRRRRRRRRRRR        RRRRRRRRRRRRRRRRR                  AAA                         JJJJJJJJJJJ
+M:::::::M             M:::::::MR::::::::::::::::R       R::::::::::::::::R                A:::A                        J:::::::::J
+M::::::::M           M::::::::MR::::::RRRRRR:::::R      R::::::RRRRRR:::::R              A:::::A                       J:::::::::J
+M:::::::::M         M:::::::::MRR:::::R     R:::::R     RR:::::R     R:::::R            A:::::::A                      JJ:::::::JJ
+M::::::::::M       M::::::::::M  R::::R     R:::::R       R::::R     R:::::R           A:::::::::A                       J:::::J  
+M:::::::::::M     M:::::::::::M  R::::R     R:::::R       R::::R     R:::::R          A:::::A:::::A                      J:::::J  
+M:::::::M::::M   M::::M:::::::M  R::::RRRRRR:::::R        R::::RRRRRR:::::R          A:::::A A:::::A                     J:::::J  
+M::::::M M::::M M::::M M::::::M  R:::::::::::::RR         R:::::::::::::RR          A:::::A   A:::::A                    J:::::j  
+M::::::M  M::::M::::M  M::::::M  R::::RRRRRR:::::R        R::::RRRRRR:::::R        A:::::A     A:::::A                   J:::::J  
+M::::::M   M:::::::M   M::::::M  R::::R     R:::::R       R::::R     R:::::R      A:::::AAAAAAAAA:::::A      JJJJJJJ     J:::::J  
+M::::::M    M:::::M    M::::::M  R::::R     R:::::R       R::::R     R:::::R     A:::::::::::::::::::::A     J:::::J     J:::::J  
+M::::::M     MMMMM     M::::::M  R::::R     R:::::R       R::::R     R:::::R    A:::::AAAAAAAAAAAAA:::::A    J::::::J   J::::::J  
+M::::::M               M::::::MRR:::::R     R:::::R     RR:::::R     R:::::R   A:::::A             A:::::A   J:::::::JJJ:::::::J  
+M::::::M               M::::::MR::::::R     R:::::R     R::::::R     R:::::R  A:::::A               A:::::A   JJ:::::::::::::JJ   
+M::::::M               M::::::MR::::::R     R:::::R     R::::::R     R:::::R A:::::A                 A:::::A    JJ:::::::::JJ     
+MMMMMMMM               MMMMMMMMRRRRRRRR     RRRRRRR     RRRRRRRR     RRRRRRRAAAAAAA                   AAAAAAA     JJJJJJJJJ       
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  
+                                                                                                                                  
+""")
